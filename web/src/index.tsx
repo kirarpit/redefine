284a5340f2b7f56290cdec7f52ea33657f91f56f
Arpit Garg <iarpitgarg@gmail.com>
2025-03-24 08:00:01 -0400

    white space on scroll issue fixed

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Redefine from "./components/Redefine";
 import "./css/tailwind.css";
+import "./css/custom.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
